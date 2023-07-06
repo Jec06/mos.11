56dd325da2fa873996b56e9a8d69a07f692fd1a1
Jec06 <120297401+Jec06@users.noreply.github.com>
2023-07-05 22:57:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openCard() {
+    var cardContainer = document.getElementById('cardContainer');
+    cardContainer.style.display = 'flex';
+  }
+  
+  function closeCard() {
+    var cardContainer = document.getElementById('cardContainer');
+    cardContainer.style.display = 'none';
+  }
